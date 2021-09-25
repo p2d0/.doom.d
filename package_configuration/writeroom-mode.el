193a439051a03d45d72103758349ee8d110c1ce2
Patriot720 <cerkin-3@yandex.ru>
2021-09-25 10:13:29 +0300

    update 10:13 Sat, 25 Sep

--- package_configuration/writeroom-mode.el
+++ package_configuration/writeroom-mode.el
@@ -1,3 +1,3 @@
 ;;; package_configuration/writeroom-mode.el -*- lexical-binding: t; -*-
 (global-writeroom-mode)
-(setq writeroom-major-modes '(text-mode org-roam-mode))
+(setq writeroom-major-modes '(markdown-mode org-mode org-roam-mode))
