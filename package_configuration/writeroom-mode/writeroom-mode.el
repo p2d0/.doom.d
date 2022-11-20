07f550941ebcc18478e56850f6c07df798becb6d
patriot720 <cerkin-3@yandex.ru>
2022-11-20 21:18:38 +0300

    updates

--- package_configuration/writeroom-mode/writeroom-mode.el
+++ package_configuration/writeroom-mode/writeroom-mode.el
@@ -1,7 +1,7 @@
 ;;; package_configuration/writeroom-mode.el -*- lexical-binding: t; -*-
-(after! org
-	(global-writeroom-mode)
-	(setq writeroom-width 100)
-	(setq +zen-text-scale 1.5)
-	(setq writeroom-major-modes '(;; markdown-mode
-																 org-mode org-roam-mode)))
+;; (after! org
+;; 	(global-writeroom-mode)
+;; 	(setq writeroom-width 100)
+;; 	(setq +zen-text-scale 1.5)
+;; 	(setq writeroom-major-modes '(;; markdown-mode
+;; 																 org-mode org-roam-mode)))
