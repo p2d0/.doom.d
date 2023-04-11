99bc02efd20d297b06fb446b49036e2fbe612dee
patriot720 <cerkin-3@yandex.ru>
2023-04-11 20:00:12 +0300

    update 20:00 Tue, 11 Apr

--- config.el
+++ config.el
@@ -183,7 +183,7 @@
 
 
 (load! "map.el")
-(load! "ssh.el" nil t)
+(load! "~/Dropbox/emacs/ssh.el" nil t)
 (load! "util.el")
 (mapc 'load (file-expand-wildcards "~/.doom.d/package_configuration/*/*.el"))
 
