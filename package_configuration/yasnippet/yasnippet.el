e77a021082f4e6dc125a1e8807f42b0468abf10a
patriot720 <cerkin-3@yandex.ru>
2024-06-09 20:00:03 +0300

    update 20:00 Sun, 09 Jun

--- package_configuration/yasnippet/yasnippet.el
+++ package_configuration/yasnippet/yasnippet.el
@@ -4,8 +4,15 @@
 
 ;; TODO fix
 
+(defun my/org-tab-conditional ()
+  (interactive)
+  (if (yas-active-snippets)
+    (yas-next-field-or-maybe-expand)
+    (org-cycle)))
 
 (after! yasnippet
 	(setq yas-wrap-around-region t)
+	(map! :map yas-keymap
+		:n [tab] #'yas-next-field-or-maybe-expand)
 	(add-hook 'yas-after-exit-snippet-hook
 		(lambda () (indent-buffer))))
