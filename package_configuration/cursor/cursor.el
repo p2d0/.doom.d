ee518fcc93f99105a804e22a3a5c2a07e87c78f7
patriot720 <cerkin-3@yandex.ru>
2024-11-16 20:00:04 +0300

    update 20:00 Sat, 16 Nov

--- package_configuration/cursor/cursor.el
+++ package_configuration/cursor/cursor.el
@@ -9,6 +9,15 @@
   (let ((path (or buffer-file-name default-directory)))
     (cursor-open path)))
 
+(defun cursor-open-project-folder ()
+  (interactive)
+  (let ((path (or buffer-file-name default-directory)))
+    (cursor-open-folder path)))
+
+(defun cursor-open-folder (path)
+	(start-process "" "*cursor*" "/run/current-system/sw/bin/cursor" (projectile-project-root) "-g"
+		path))
+
 (map!
   :leader
   "fc" #'cursor-open-current-file)
