a17908dcef004c020821d3ad59f447db9b92b88f
patriot720 <cerkin-3@yandex.ru>
2024-12-20 20:00:05 +0300

    update 20:00 Fri, 20 Dec

--- package_configuration/tailwind/tailwind.el
+++ package_configuration/tailwind/tailwind.el
@@ -1,3 +1,7 @@
 ;;; nixos/editors/.doom.d/package_configuration/tailwind/tailwind.el -*- lexical-binding: t; -*-
 
 (use-package! lsp-tailwindcss :after lsp-mode)
+
+(after! lsp-mode
+	(set-company-backend! 'web-mode 'company-capf)
+	)
