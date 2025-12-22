f5268ac01eec2cd13879283b095529bc0eaace2b
patriot720 <cerkin-3@yandex.ru>
2025-12-22 20:00:05 +0300

    update 20:00 Mon, 22 Dec

--- package_configuration/gptel/gptel_2.el
+++ package_configuration/gptel/gptel_2.el
@@ -10,13 +10,13 @@
 (set-popup-rule! "^\\*OpenRouter\\*" :side 'right :size 0.4 :select 1)
 
 ;; OpenRouter offers an OpenAI compatible API
-(setq gptel-model 'deepseek/deepseek-v3.2
+(setq gptel-model 'google/gemini-3-flash-preview
 	gptel-backend (gptel-make-openai "OpenRouter"               ;Any name you want
 									:host "openrouter.ai"
 									:endpoint "/api/v1/chat/completions"
 									:stream t
 									:key (with-temp-buffer (insert-file-contents "/etc/nixos/modules/nixos/editors/.doom.d/package_configuration/gptel/.env") (s-trim (buffer-string) ))
-									:models '(z-ai/glm-4.5-air deepseek/deepseek-v3.2 qwen/qwen3-235b-a22b google/gemini-2.0-flash-001 deepseek/deepseek-chat-v3-0324 qwen/qwen-2.5-coder-32b-instruct meta-llama/llama-3.3-70b-instruct)))
+									:models '(google/gemini-3-flash-preview z-ai/glm-4.5-air deepseek/deepseek-v3.2 qwen/qwen3-235b-a22b google/gemini-2.0-flash-001 deepseek/deepseek-chat-v3-0324 qwen/qwen-2.5-coder-32b-instruct meta-llama/llama-3.3-70b-instruct)))
 
 	(setq gptel-directives '((default
 														. "You are a large language model living in Emacs and a helpful assistant. Respond concisely.")
